3a5bf2760063793adf88ee414adc775940fdf3c5
s0mar1 <jiseong0310@gmail.com>
2025-06-18 09:31:46 +0900

    Fix fetch API URL template

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 // src/api/index.js
 export const fetchSummonerDataAPI = async (region, rawName) => {
-  const response = await fetch(`/api/summoner/<span class="math-inline">\{region\}/</span>{rawName}`);
+  const response = await fetch(`/api/summoner/${region}/${rawName}`);
   if (!response.ok) {
     const errorData = await response.json();
     throw new Error(errorData.error || 'API Error');
